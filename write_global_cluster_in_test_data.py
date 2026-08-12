5b8362063907e0a5508f6ca42823550838b4ab9b
marconobile <marconobile.007.mn@gmail.com>
2026-08-12 15:39:41 +0200

    add support both for train and denovo samples

--- write_global_cluster_in_test_data.py
+++ write_global_cluster_in_test_data.py
@@ -9,6 +9,14 @@ write_global_cluster_in_test_data.py \
 import argparse
 import numpy as np
 
+# get cmd line inputs
+parser = argparse.ArgumentParser()
+parser.add_argument("--input-npz")
+parser.add_argument("--output-npz")
+args = parser.parse_args()
+input_npz = args.input_npz
+output_npz = args.output_npz
+
 # seq = 'SSVYITVELAIAVLAILGNVLVCWAVWLNSNLQNVTNYFVVSLAAADIAVGVLAIPFAITISTGFCAACHGCLFIACFVLVLTQSSIFSLLAIAIDRYIAIRIPLRYNGLVTGTRAKGIIAICWVLSFAIGLTPMLGWNNCGQPKEGKNHSQGCGEGQVACLFEDVVPMNYMVYFNFFACVLVPLLLMLGVYLRIFLAARRQLKQMESQPLPGERARSTLQKEVHAAKSLAIIVGLFALCWLPLHIINCFTFFCPDCSHAPLWLMYLAIVLSHTNSVVNPFIYAYRIREFRQTFRKIIRS'
 
 # step 1: load TRAIN data, must be train since onto it it is defined the global clusters mapping
@@ -55,27 +63,22 @@ for frame in data['residue_cluster_ids']:
 data['res_idx_and_glob_cluster_id_per_frame'] = np.array(res_idx_and_glob_cluster_id) # (49996, 300)
 data['atom_idx_and_glob_cluster_id_per_frame'] = data['res_idx_and_glob_cluster_id_per_frame'][:, new_atom_resids] # (49996, 2338)    
 
-
-# load test data now
-parser = argparse.ArgumentParser()
-parser.add_argument("--input-npz")
-parser.add_argument("--output-npz")
-args = parser.parse_args()
-
-input_npz = args.input_npz
-output_npz = args.output_npz
-
+# load test data 
 TEST_DATA = dict(np.load(input_npz))
-res_idx_and_glob_cluster_id = []
-for frame in TEST_DATA['residue_cluster_ids']:
+if 'labels' in TEST_DATA: res_key = 'labels'
+else: res_key = 'residue_cluster_ids'
+if 'labels' in TEST_DATA and 'residue_cluster_ids' in TEST_DATA: raise ValueError("ERROR: both 'labels' and 'residue_cluster_ids' are present in TEST_DATA, be sure of what to use")
+
+res_idx_and_glob_cluster_id_test_data = []
+for frame in TEST_DATA[res_key]:
     for_this_frame = []
     for res_idx, cluster_id in enumerate(frame):
-        glob_cluster_id = res_idx_to_glob_cluster_ids[(res_idx, cluster_id)]
+        glob_cluster_id = res_idx_to_glob_cluster_ids[(res_idx, int(cluster_id))]
         for_this_frame.append(glob_cluster_id)
-    res_idx_and_glob_cluster_id.append(for_this_frame)
+    res_idx_and_glob_cluster_id_test_data.append(for_this_frame)
 
 TEST_DATA["res_idx_and_glob_cluster_id_per_frame"] = np.array(
-    res_idx_and_glob_cluster_id
+    res_idx_and_glob_cluster_id_test_data
 )    
 
 TEST_DATA["atom_idx_and_glob_cluster_id_per_frame"] = TEST_DATA[
